765cf082d3b1743b8ea9f9e0fb677f5c1e349cb7
Maarten van Middelaar <maarten@duara.nl>
2023-05-23 19:03:05 +0300

    add files

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import unittest
+from  parameterized import parameterized
+import utils
+
+class TestAccessNestedMap(unittest.TestCase):
+  
+  
+  @parameterized.expand([
+     ({"a": 1}, ("a",), 1),
+     ({"a": {"b": 2}}, ("a",), {"b":2}),
+     ({"a": {"b": 2}}, ("a", "b",), 2)
+  ])
+  def test_access_nested_map(self, nested_map, path, expected_result):
+     self.assertEqual(utils.access_nested_map(nested_map, path), expected_result);
+
+
+
